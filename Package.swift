9460a915f9eb702e2853bd0c3422e36abb8d6a5a
xcriss <cristian.oprea@up2date.ro>
2026-03-17 14:36:16 +0200

    Fix SPM: use local xcframework path instead of release URL

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RomanianEIDSDK",
-            url: "https://github.com/Up2dateSoftware/EidRomaniaSDK/releases/download/v1.4.0/RomanianEIDSDK.xcframework.zip",
-            checksum: "434015bdca0d244ae6a8b763ebbb9304ea5dbd0ca0c7642161a88c9e5477dc0c"
+            path: "ios/RomanianEIDSDK.xcframework.zip"
         )
     ]
 )
