268dd22ba5f0eab5c9c8d43432f31b7c443f0b0d
Cristian Oprea <cristian.oprea@up2date.ro>
2026-05-18 10:46:50 +0300

    Fix OpenSSL signing: clean metadata, re-sign all layers

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "OpenSSL",
             url: "https://github.com/Up2dateSoftware/EidRomaniaSDK/releases/download/1.4.19/OpenSSL.xcframework.zip",
-            checksum: "47402a4024f7b7e557ebf3035e0b09e2ce95f15edac257686b76a31b9ffc2d09"
+            checksum: "55a6be6398647e2c3fddefe14ae1096ed599530dbfd1a5d700b9f9e3cbdf7bf9"
         )
     ]
 )
